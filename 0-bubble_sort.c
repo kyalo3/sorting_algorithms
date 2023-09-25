13e978087e130b31c381d5888ca5fc6d89fb4926
kyalo3 <kyalokimeu0@gmail.com>
2023-09-25 15:25:27 +0300

    0. Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,45 +1,45 @@
-
 #include "sort.h"
 
 void bubble_sort(int *array, size_t size)
 {
-    int swapped, temp;
-    size_t i, j, k;
+	int swapped, temp;
+	size_t i, j, k;
 	/*size_t i, j; // Declare loop variables here*/
 
-    for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-        swapped = 0;
+		swapped = 0;
 
-        for (j = 0; j < size - 1 - i; j++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
-            if (array[j] > array[j + 1])
+			if (array[j] > array[j + 1])
 				{
-                /* Swap array[j] and array[j + 1] */
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+				/* Swap array[j] and array[j + 1] */
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 
-                swapped = 1; /* Set swapped flag */
+				swapped = 1; /* Set swapped flag */
 				for (k = 0; k < size; k++)
 				{
-					if (k == size -1)
+					if (k == size - 1)
 					{
 						printf("%d", array[k]);
 					}
-					else {
+					else
+					{
 						printf("%d, ", array[k]);
 					}
 				}
 				printf("\n");
-            }
+			}
 
 		}
 		/*printf("%d, ", array[i], array[j]);*/
-        /* If no swaps were made in this pass, the array is sorted */
-        if (!swapped)
+		/* If no swaps were made in this pass, the array is sorted */
+		if (!swapped)
 		{
-            break;
-        }
-    }
+			break;
+		}
+	}
 }
