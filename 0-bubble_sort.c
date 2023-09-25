40ffdb4e2d9a9939395c1acc2f36bfede9888e96
kyalo3 <kyalokimeu0@gmail.com>
2023-09-25 14:21:13 +0300

    bbb

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,24 +3,25 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    int swapped;
+    int swapped, temp;
+    size_t i, j, k;
 	/*size_t i, j; // Declare loop variables here*/
 
-    for (size_t i = 0; i < size - 1; i++)
+    for (i = 0; i < size - 1; i++)
 	{
         swapped = 0;
 
-        for (size_t j = 0; j < size - 1 - i; j++)
+        for (j = 0; j < size - 1 - i; j++)
 		{
             if (array[j] > array[j + 1])
 				{
                 /* Swap array[j] and array[j + 1] */
-                int temp = array[j];
+                temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
 
                 swapped = 1; /* Set swapped flag */
-				for (size_t k = 0; k < size; k++)
+				for (k = 0; k < size; k++)
 				{
 					if (k == size -1)
 					{
