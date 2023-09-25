4ba88886fc3e11c32306b04cd4dd111a9261e5d1
kyalo3 <kyalokimeu0@gmail.com>
2023-09-25 23:40:02 +0300

    2. Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,29 +10,29 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
-    int temp;
+	int temp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min_idx = i;
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
 
-        for (j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-        }
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
 
-        if (min_idx != i)
-        {
-            /* Swap array[i] and array[min_idx] */
-            temp = array[i];
-            array[i] = array[min_idx];
-            array[min_idx] = temp;
+		if (min_idx != i)
+		{
+			/* Swap array[i] and array[min_idx] */
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
 
-            print_array(array, size); /* Print the array after each swap */
-        }
-    }
-}	
+			print_array(array, size); /* Print the array after each swap */
+		}
+	}
+}
