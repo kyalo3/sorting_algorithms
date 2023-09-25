fcf7e26da44d4fb06340c2caa2e2e0b347983fd3
kyalo3 <kyalokimeu0@gmail.com>
2023-09-25 23:37:01 +0300

    2. Selection sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-
-#include "sort.h"
-
-void bubble_sort(int *array, size_t size)
-{
-    int swapped;
-	/*size_t i, j; // Declare loop variables here*/
-
-    for (size_t i = 0; i < size - 1; i++)
-	{
-        swapped = 0;
-
-        for (size_t j = 0; j < size - 1 - i; j++)
-		{
-            if (array[j] > array[j + 1])
-				{
-                /* Swap array[j] and array[j + 1] */
-                int temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-
-                swapped = 1; /* Set swapped flag */
-				for (size_t k = 0; k < size; k++)
-				{
-					if (k == size -1)
-					{
-						printf("%d", array[k]);
-					}
-					else {
-=======
 #include "sort.h"
 /**
  * bubble_sort - Sorts an array of integers in ascending order
@@ -40,8 +9,11 @@ void bubble_sort(int *array, size_t size)
 void bubble_sort(int *array, size_t size)
 {
 	int swapped, temp;
-	size_t i, j, k;
+	size_t i, j;
 	/*size_t i, j; // Declare loop variables here*/
+	
+    if (array == NULL || size < 2)
+		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
@@ -65,23 +37,10 @@ void bubble_sort(int *array, size_t size)
 					}
 					else
 					{
->>>>>>> main
 						printf("%d, ", array[k]);
 					}
 				}
 				printf("\n");
-<<<<<<< HEAD
-            }
-
-		}
-		/*printf("%d, ", array[i], array[j]);*/
-        /* If no swaps were made in this pass, the array is sorted */
-        if (!swapped)
-		{
-            break;
-        }
-    }
-=======
 			}
 
 		}
@@ -91,5 +50,4 @@ void bubble_sort(int *array, size_t size)
 			break;
 		}
 	}
->>>>>>> main
 }
