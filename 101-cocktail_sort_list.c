3c7d71868c9f21e0a2c471b89bc135f3f97dbf7f
Mally13 <mallymutuku13@gmail.com>
2023-09-27 02:01:39 +0300

    Task 103

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,72 +1,87 @@
 #include "sort.h"
-
-
 /**
- * swap_nodes - Swaps two nodes in a doubly linked list
+ * swap_node_behind - Swaps a node with a node behind it
  * @list: Pointer to the head of the doubly linked list
- * @node1: Pointer to the first node to be swapped
- * @node2: Pointer to the second node to be swapped
+ * @tail: Pointer to the tail of the double linked list
+ * @shaker: Pointer to be swapped
  */
-void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker)
 {
-	if (!node1 || !node2)
-        return;
-	if (node1->prev)
-		node1->prev->next = node2;
+	listint_t *tmp = (*shaker)->prev;
+
+	if ((*shaker)->next != NULL)
+		(*shaker)->next->prev = tmp;
 	else
-		*list = node2;
-
-	if (node2->next)
-		node2->next->prev = node1;
-
-	node1->next = node2->next;
-	node2->prev = node1->prev;
-	node1->prev = node2;
-	node2->next = node1;
+		*tail = tmp;
+	tmp->next = (*shaker)->next;
+	(*shaker)->prev = tmp->prev;
+	if (tmp->prev != NULL)
+		tmp->prev->next = *shaker;
+	else
+		*list = *shaker;
+	(*shaker)->next = tmp;
+	tmp->prev = *shaker;
+	*shaker = tmp;
 }
 /**
- * cocktail_sort_list -  a function that sorts a doubly linked list of integers in
- * ascending order using the Cocktail shaker sort algorithm
- * @list: Double linked list to sort
+ * swap_node_ahead- Swaps a node with a node ahead of it
+ * @list: Pointer to the head of the doubly linked list
+ * @tail: Pointer to the tail of the double linked list
+ * @shaker: Pointer to be swapped
+ */
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker)
+{
+	listint_t *tmp = (*shaker)->next;
+
+	if ((*shaker)->prev != NULL)
+		(*shaker)->prev->next = tmp;
+	else
+		*list = tmp;
+	tmp->prev = (*shaker)->prev;
+	(*shaker)->next = tmp->next;
+	if (tmp->next != NULL)
+		tmp->next->prev = *shaker;
+	else
+		*tail = *shaker;
+	(*shaker)->prev = tmp;
+	tmp->next = *shaker;
+	*shaker = tmp;
+}
+/**
+ * cocktail_sort_list -  a function that sorts a doubly linked list
+ * of integers in ascending order using the Cocktail shaker sort algorithm
+ * @list: Double linked list to be sorted
  */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *left, *right;
-	int swapped;
+	listint_t *tail, *shaker;
+	int swapped = 1;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
+	for (tail = *list; tail->next != NULL;)
+		tail = tail->next;
 	do {
 		swapped = 0;
-		left = *list;
-		right = NULL;
-
-		while (left->next != right)
+		for (shaker = *list; shaker != tail; shaker = shaker->next)
 		{
-			if (left->n > left->next->n)
+			if (shaker->n > shaker->next->n)
 			{
-				swap_nodes(list, left, left->next);
-				print_list(*list);
+				swap_node_ahead(list, &tail, &shaker);
+				print_list((const listint_t *)*list);
 				swapped = 1;
 			}
-			left = left->next;
 		}
-		if (!swapped)
-			break;
-		swapped = 0;
-		right = left;
-		left = left->prev;
-
-		while (right->prev != left)
+		for (shaker = shaker->prev; shaker != *list;
+		shaker = shaker->prev)
 		{
-			if (right->n < right->prev->n)
+			if (shaker->n < shaker->prev->n)
 			{
-				swap_nodes(list, right->prev, right);
-				print_list(*list);
+				swap_node_behind(list, &tail, &shaker);
+				print_list((const listint_t *)*list);
 				swapped = 1;
 			}
-			right = right->prev;
 		}
 	} while (swapped);
 }
