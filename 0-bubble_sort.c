425361c98e246d40cf9455628a2f08c6ac9d3f11
Mally13 <mallymutuku13@gmail.com>
2023-09-26 10:39:49 +0300

    Task 0 fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,8 +9,7 @@
 void bubble_sort(int *array, size_t size)
 {
 	int swapped, temp;
-	size_t i, j, k;
-	/*size_t i, j; // Declare loop variables here*/
+	size_t i, j;
 
 	for (i = 0; i < size - 1; i++)
 	{
@@ -19,29 +18,18 @@ void bubble_sort(int *array, size_t size)
 		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
-				{
-				/* Swap array[j] and array[j + 1] */
+			{
+
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-
-				swapped = 1; /* Set swapped flag */
-				for (k = 0; k < size; k++)
-				{
-					if (k == size - 1)
-					{
-						printf("%d", array[k]);
-					}
-					else
-					{
-						printf("%d, ", array[k]);
-					}
-				}
+				print_array(array, size);
+				swapped = 1;
 				printf("\n");
 			}
 
 		}
-		/* If no swaps were made in this pass, the array is sorted */
+
 		if (!swapped)
 		{
 			break;
